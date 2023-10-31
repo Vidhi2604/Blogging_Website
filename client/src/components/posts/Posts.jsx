2712252cc5b0e85e81e715dc1d95aa04a26e6066
Vidhi Sahai <72317428+Vidhi2604@users.noreply.github.com>
2023-10-31 12:56:08 +0530

    Add files via upload

---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,17 @@
+import "./posts.css";
+import Post from "../post/Post";
+
+export default function Posts() {
+  return (
+    <div className="posts">
+        <Post img="https://img.freepik.com/free-photo/beautiful-shot-small-lake-with-wooden-rowboat-focus-amazing-clouds-sky_181624-2044.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=sph"/>
+        <Post img="https://img.freepik.com/free-photo/pink-water-lillie-s-thailand_181624-27895.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=sph" />
+        <Post img="https://img.freepik.com/free-photo/beautiful-girls-having-fun-beach_23-2148594327.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=sph" />
+        <Post img="https://img.freepik.com/free-photo/girl-smelling-flowers-standing-street_1304-4185.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=ais" />
+        <Post img="https://img.freepik.com/free-photo/agency-young-adult-profession-stressed-black_1134-1279.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=ais" />
+        <Post img="https://img.freepik.com/free-photo/green-mountains-ancient-indian-village-malana-state-himachal-pradesh_181624-17940.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=ais" />
+        <Post img="https://img.freepik.com/free-photo/dry-trees-rocky-landscape-with-lake-background_1150-18314.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=ais" />
+        <Post img="https://img.freepik.com/free-photo/beautiful-mountain-landscape_1136-385.jpg?size=626&ext=jpg&uid=R114002739&ga=GA1.1.1846457180.1689760031&semt=ais" />
+    </div>
+  )
+}
